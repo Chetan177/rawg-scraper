0dfa7846e6b1008de1d63d42438f36ce34ec1e0b
chetan177 <chetanpandey177@gmail.com>
2021-01-20 00:12:11 +0530

    add data directly to the mongodb

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
-const fs = require('fs');
+const { MongoClient } = require('mongodb');
 
+// const DB_URI = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority"
+const DB_URI = "mongodb://localhost:27017/"
+const DB_Name = "gamesdb"
 
 const dataDir = "data/page-"
 // 'https://api.rawg.io/api/games?page=1?page_size=40'
@@ -9,22 +12,42 @@ const dataDir = "data/page-"
 const startPage = 1;
 const endPage = 2;
 
+const dbClient = new MongoClient(DB_URI);
+
 const main = async function () {
+    await dbClient.connect();
+    databasesList = await dbClient.db().admin().listDatabases();
+
     for (i = startPage; i <= endPage; i++) {
-        console.log(`page no ${i}` + dataDir + `${i}.json`);
+
         const URL = `https://api.rawg.io/api/games?page=${i}&page_size=40`
 
         await axios.get(URL).then(function (response) {
-            const data = JSON.stringify(response.data, null, 2);
-            fs.writeFileSync(dataDir + `${i}.json`, data, (err) => {
-                if (err) throw err;
-                console.log('Data written to file');
+
+            let data = response.data.results
+            data.forEach(function (element, index) {
+                data[index]._id = String(element.id) + element.slug
             });
+
+            insertIntoDB(dbClient, data)
+
         }).catch(function (error) {
             console.log(error);
         })
 
     }
+
+    dbClient.close();
+}
+
+const insertIntoDB = async function (client, data) {
+    var dbObj = client.db(DB_Name)
+    dbObj.collection("games").insertMany(data, function (err, res) {
+        if (err && err.code === 11000) { console.error("got duplicate key") }
+        if (err) { console.error(err) }
+        console.log(`${res.insertedCount} doc added`);
+    });
+
 }
 
 main();
